3a4e90e9253494c2489192a42ab085ffb6631109
MASTERCOMPYUTER <126121596+MASTERCOMPYUTER@users.noreply.github.com>
2023-03-04 02:34:45 +0500

    first commit

---
+++ OnlineHacKing/0.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo ""
+echo ""
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;90m\e[0m\e[1;92m  !!       Welcome to DISRUPTIVE STUDENT      !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ---------------------------------------- \e[1;91m\e[0m'
+echo ""
+echo -e ""
+# echo -e " \e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m \e[95m How To Install And Use This Cloud Linux RDP. Read This Article \e[0m"
+# echo ""
+# echo -e
+# echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Article Link :\e[0m\e[1;36m https://www.onlinehacking.org/cloud-linux-rdp-lifetime-free-for-google-cloud-console/  "
+echo ""
+echo -e
+# echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Video Link :\e[0m\e[1;36m coming soon.....  "
+echo ""
+echo ""
+echo ""
+echo ""
+read -p $'   \e[1;40m\e[31m[\e[32m*\e[31m]\e[32m Click Enter to Continue  \e[1;91m : \e[0m' option
+echo""
+echo""
+clear
+echo ""
